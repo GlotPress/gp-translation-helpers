057ed3efdd9b3ade8b2f6262c793a5ebea1d8154
Tosin Oguntuyi <tcubed03@gmail.com>
2022-02-08 18:17:36 +0100

    Refactor code to make modal work on each reject button on each translation row

--- js/reject-feedback.js
+++ js/reject-feedback.js
@@ -1,11 +1,10 @@
 (function( $, $gp ) {
 	$( document ).ready(
 		function($) {
-
 			let reject_feedback_form =
 			'<div id="reject-feedback-form" style="display:none;">' +
 			'<form>' +
-			'<h3>Reason</h3>' +
+			'<h3 class="modal-reason-title">Reason</h3>' +
 			'<div class="modal-item"><input type="checkbox" name="reject_reason" value="style" /><label>Style Guide </label></div>' +
 			'<div class="modal-item"><input type="checkbox" name="reject_reason" value="grammar" /><label>Grammar </label></div>' +
 			'<div class="modal-item"><input type="checkbox" name="reject_reason" value="branding" /><label>Branding </label></div>' +
@@ -22,7 +21,38 @@
 
 			$( "body" ).append( reject_feedback_form );
 
-			function postComment( reject_reason, comment ){
+		}
+	);
+
+	$gp.editor.hooks.set_status_rejected = function() {
+		let button = $( this );
+		let status = 'rejected';
+		rejectWithFeedback( button, status );
+
+	}
+
+	function rejectWithFeedback( button, status ){
+        // Show thickbox modal
+		tb_show( 'Reject with Feedback', '#TB_inline?inlineId=reject-feedback-form' );
+
+		let reject_reason = [];
+		let comment       = '';
+
+		$( 'body' ).off().on(
+			'click' ,
+			'#gp_reject_btn',
+			function(e){
+				e.preventDefault();
+				e.stopImmediatePropagation();
+
+				$( 'input[name="reject_reason"]:checked' ).each(
+					function() {
+						reject_reason.push( this.value );
+					}
+				);
+
+				comment = $( 'textarea[name="reject_comment"]' ).val();
+
 				reject_data                = {};
 				reject_data.locale_slug    = $gp_reject_feedback_settings.locale_slug;
 				reject_data.reason         = reject_reason;
@@ -35,66 +65,24 @@
 					data: reject_data,
 					_ajax_nonce: $gp_reject_feedback_settings.nonce,
 				};
+
 				$.ajax(
 					{
 						type: 'POST',
 						url: $gp_reject_feedback_settings.url,
-						data: data}
+						data: data
+					}
 				).done(
 					function( response ){
-						// TODO: Handling response
+						$gp.editor.set_status( button, status );
+						$( 'input[name="reject_reason"]' ).prop( "checked", false );
+						$( 'textarea[name="reject_comment"]' ).val( '' );
+						$( '#TB_closeWindowButton' ).click();
 					}
 				);
 			}
+		);
 
-			$( 'body' ).on(
-				'click' ,
-				'#gp_reject_btn',
-				function(e){
-					e.preventDefault();
-
-					let reject_reason = [];
-					$( 'input[name="reject_reason"]:checked' ).each(
-						function() {
-							reject_reason.push( this.value );
-						}
-					);
-
-					let comment = $( 'textarea[name="reject_comment"]' ).val();
-
-					data = {
-						translation_id: $gp.editor.current.translation_id,
-						status: 'rejected',
-						_gp_route_nonce: $( 'button.reject' ).data( 'nonce' ),
-					};
-
-					$.ajax(
-						{
-							type: 'POST',
-							url: $gp_editor_options.set_status_url,
-							data: data,
-							success: function( response ) {
-								if ( reject_reason || comment ) {
-									postComment( reject_reason, comment );
-								}
-								$gp.notices.success( 'Translation Rejected!' );
-								$gp.editor.replace_current( response );
-								$gp.editor.next();
-								$( '#TB_window, #TB_overlay' ).fadeOut();
-
-							},
-							error: function( xhr, msg ) {
-								msg = xhr.responseText ? 'Error: ' + xhr.responseText : 'Error setting the status!';
-								$gp.notices.error( msg );
-							}
-						}
-					);
-				}
-			);
-		}
-	);
-	$gp.editor.hooks.set_status_rejected = function() {
-		tb_show( 'Reject with Feedback', '#TB_inline?inlineId=reject-feedback-form' );
 	}
 }(jQuery, $gp)
 );
