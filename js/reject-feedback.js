87b2a95a683f8be9a5b723e81113e3df3f09ce33
Alex Kirk <akirk@users.noreply.github.com>
2022-02-24 14:43:19 +0100

    Remove space

--- js/reject-feedback.js
+++ js/reject-feedback.js
@@ -7,12 +7,12 @@
 			'<form>' +
 			'<h3 class="feedback-reason-title">Reason</h3>' +
 			'<ul class="feedback-reason-list">' +
-			'<li><label><input type="checkbox" name="feedback_reason" value="style" /> Style Guide</label></li>' +
-			'<li><label><input type="checkbox" name="feedback_reason" value="grammar" /> Grammar</label></li>' +
-			'<li><label><input type="checkbox" name="feedback_reason" value="branding" /> Branding</label></li>' +
-			'<li><label><input type="checkbox" name="feedback_reason" value="glossary" /> Glossary</label></li>' +
-			'<li><label><input type="checkbox" name="feedback_reason" value="punctuation" /> Punctuation</label></li>' +
-			'<li><label><input type="checkbox" name="feedback_reason" value="typo" /> Typo</label></li></ul>' +
+			'<li><label><input type="checkbox" name="feedback_reason" value="style" />Style Guide</label></li>' +
+			'<li><label><input type="checkbox" name="feedback_reason" value="grammar" />Grammar</label></li>' +
+			'<li><label><input type="checkbox" name="feedback_reason" value="branding" />Branding</label></li>' +
+			'<li><label><input type="checkbox" name="feedback_reason" value="glossary" />Glossary</label></li>' +
+			'<li><label><input type="checkbox" name="feedback_reason" value="punctuation" />Punctuation</label></li>' +
+			'<li><label><input type="checkbox" name="feedback_reason" value="typo" />Typo</label></li></ul>' +
 			'<div class="feedback-comment">' +
 				'<label>Comment </label>' +
 				'<textarea name="feedback_comment"></textarea>' +
