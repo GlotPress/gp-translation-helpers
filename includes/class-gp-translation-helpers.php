53193b1252475b95607668c9751dd9d8fea70c0d
Tosin Oguntuyi <tcubed03@gmail.com>
2022-03-09 14:55:18 +0100

    Check if reject reason is valid

--- includes/class-gp-translation-helpers.php
+++ includes/class-gp-translation-helpers.php
@@ -368,7 +368,13 @@ class GP_Translation_Helpers {
 		$translation_id_array = array_map( array( $helper_discussion, 'sanitize_translation_id' ), $_POST['data']['translation_id'] );
 		$original_id_array    = array_map( array( $helper_discussion, 'sanitize_original_id' ), $_POST['data']['original_id'] );
 		$reject_reason        = ! empty( $_POST['data']['reason'] ) ? $_POST['data']['reason'] : array( 'other' );
-		$reject_reason        = array_map( 'sanitize_text_field', $reject_reason );
+		$all_reject_reasons   = array_keys( Helper_Translation_Discussion::$reject_reasons );
+		$reject_reason        = array_filter(
+			$reject_reason,
+			function( $reason ) use ( $all_reject_reasons ) {
+				return in_array( $reason, $all_reject_reasons );
+			}
+		);
 		$reject_comment       = sanitize_text_field( $_POST['data']['comment'] );
 
 		if ( ! $locale_slug || ! $translation_id_array || ! $original_id_array || ( ! $reject_reason && ! $reject_comment ) ) {
