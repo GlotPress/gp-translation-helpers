f01c1a10d4cda89dba803ccf0f170b738eefea58
Tosin Oguntuyi <tcubed03@gmail.com>
2022-06-24 17:02:47 +0100

    Replace hardcoded tmp folder path with environment variable for tmp directory

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -22,11 +22,12 @@ if ( ! defined( 'GP_TESTS_PERMALINK_STRUCTURE_WITH_TRAILING_SLASH' ) ) {
 	define( 'GP_TESTS_PERMALINK_STRUCTURE_WITH_TRAILING_SLASH', '/%postname%/' );
 }
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' ) . '/wordpress-tests-lib';
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
 
 if ( ! $_tests_dir ) {
-	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' );
 }
+$_tests_dir .= '/wordpress-tests-lib';
 
 if ( ! file_exists( "{$_tests_dir}/includes/functions.php" ) ) {
 	echo "Could not find {$_tests_dir}/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
