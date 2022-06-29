e9b46ae6b69ded72f5a58316d7edb6b3d2a2c9f9
Alex Kirk <akirk@users.noreply.github.com>
2022-06-29 15:58:19 +0200

    Don't always append /wordpress-tests-lib

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -25,9 +25,8 @@ if ( ! defined( 'GP_TESTS_PERMALINK_STRUCTURE_WITH_TRAILING_SLASH' ) ) {
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
 if ( ! $_tests_dir ) {
-	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' );
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
-$_tests_dir .= '/wordpress-tests-lib';
 
 if ( ! file_exists( "{$_tests_dir}/includes/functions.php" ) ) {
 	echo "Could not find {$_tests_dir}/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
