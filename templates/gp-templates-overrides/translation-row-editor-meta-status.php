9b3bc6f0bced20766031df1be3492f2e907e1f21
Tosin Oguntuyi <tcubed03@gmail.com>
2022-07-04 11:27:01 +0100

    Move feedback form into own template file

--- templates/gp-templates-overrides/translation-row-editor-meta-status.php
+++ templates/gp-templates-overrides/translation-row-editor-meta-status.php
@@ -8,26 +8,7 @@
 
 ?>
 <dl>
-<details>
-	<summary class="feedback-summary">Give feedback</summary>
-	<div id="feedback-form">
-		<form>
-			<h3 class="feedback-reason-title">Reason</h3>
-			<ul class="feedback-reason-list">
-				<li><label><input type="checkbox" name="feedback_reason" value="style">Style Guide</label></li>
-				<li><label><input type="checkbox" name="feedback_reason" value="grammar">Grammar</label></li>
-				<li><label><input type="checkbox" name="feedback_reason" value="branding">Branding</label></li>
-				<li><label><input type="checkbox" name="feedback_reason" value="glossary">Glossary</label></li>
-				<li><label><input type="checkbox" name="feedback_reason" value="punctuation">Punctuation</label></li>
-				<li><label><input type="checkbox" name="feedback_reason" value="typo">Typo</label></li>
-			</ul>
-			<div class="feedback-comment">
-				<label>Comment </label>
-				<textarea name="feedback_comment"></textarea>
-			</div>
-		</form>
-	</div>
-</details>
+	<?php gp_tmpl_load( 'translation-row-editor-meta-feedback', get_defined_vars(), dirname( dirname( __FILE__ ) ) ); ?>
 	<dt><?php _e( 'Status:', 'glotpress' ); ?></dt>
 	<dd>
 		<?php echo display_status( $translation->translation_status ); ?>
