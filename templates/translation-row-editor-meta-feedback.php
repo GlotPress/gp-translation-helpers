004f7843fc0e4097a7705b0c0d5b5c8849aa062c
Jesús Amieiro <1667814+amieiro@users.noreply.github.com>
2022-07-21 15:33:22 +0200

    In the simple rejection, set the checkbox and the info icon in the same line

--- templates/translation-row-editor-meta-feedback.php
+++ templates/translation-row-editor-meta-feedback.php
@@ -12,11 +12,7 @@
 			foreach ( $reject_reasons as $key => $reason ) :
 				?>
 					<li>
-						<label class="tooltip" title="<?php echo esc_attr( $reason['explanation'], 'glotpress' ); ?>">
-							<input type="checkbox" name="feedback_reason" value="<?php echo esc_attr( $key, 'glotpress' ); ?>" />
-							<?php echo esc_html( $reason['name'], 'glotpress' ); ?><span class="tooltip dashicons dashicons-info" title="<?php echo esc_attr( $reason['explanation'], 'glotpress' ); ?>"></span>
-						</label>
-
+						<label class="tooltip" title="<?php echo esc_attr( $reason['explanation'], 'glotpress' ); ?>"><input type="checkbox" name="feedback_reason" value="<?php echo esc_attr( $key, 'glotpress' ); ?>" /><?php echo esc_html( $reason['name'], 'glotpress' ); ?></label><span class="tooltip dashicons dashicons-info" title="<?php echo esc_attr( $reason['explanation'], 'glotpress' ); ?>"></span>
 					</li>
 			<?php endforeach; ?>
 			</ul>
