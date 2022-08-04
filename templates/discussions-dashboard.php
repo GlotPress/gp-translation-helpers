6941d285de301f27839e189a2de0dc6d86da3606
Tosin Oguntuyi <tcubed03@gmail.com>
2022-08-04 19:21:38 +0100

    Add original string to table

--- templates/discussions-dashboard.php
+++ templates/discussions-dashboard.php
@@ -13,24 +13,33 @@ gp_tmpl_header();
 <table id="translations" class="translations clear">
 	<thead>
 	<tr>
-		<th>Author</th>
+		<th>Original string</th>
 		<th>Comment</th>
+		<th>Author</th>
 		<th>Submitted on</th>
 	</tr>
 	</thead>
 	<tbody>
 		<?php
 		foreach ( $comments as $discussion_comment ) {
+			$original_id = Helper_Translation_Discussion::get_original_from_post_id( $discussion_comment->comment_post_ID );
+			$original    = GP::$original->get( $original_id );
 			echo '<tr>' .
-			 '<td>' . esc_html( $discussion_comment->comment_author ) . '</td>' .
+			 '<td>' . esc_html( $original->singular ) . '</td>' .
 			 '<td><a href="' . esc_url( get_comment_link( $discussion_comment ) ) . '">' . esc_html( $discussion_comment->comment_content ) . '</a></td>' .
+			 '<td>' . esc_html( $discussion_comment->comment_author ) . '</td>' .
 			 '<td>' . esc_html( $discussion_comment->comment_date ) . '</td>' .
 			 '</tr>';
 
 		}
 		?>
+		
 	</tbody>
 	
 </table>
+
+
+	</li>
+</ul>
 <?php
 gp_tmpl_footer();
