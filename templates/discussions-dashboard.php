0357b0b8528d6dba640e00392405ede8d2d32396
Alex Kirk <akirk@users.noreply.github.com>
2022-08-05 11:11:19 +0200

    Try a normal comment display

--- templates/discussions-dashboard.php
+++ templates/discussions-dashboard.php
@@ -10,6 +10,67 @@ gp_breadcrumb( $breadcrumb );
 gp_tmpl_header();
 
 ?>
+<style>
+	html { scroll-behavior: smooth; }
+</style>
+
+<?php
+
+$comments_by_post_id = array();
+$last_comment_by_post_id = array();
+
+foreach ( $comments as $_comment ) {
+	$original_id = Helper_Translation_Discussion::get_original_from_post_id( $_comment->comment_post_ID );
+	if ( ! isset( $comments_by_post_id[ $_comment->comment_post_ID ] ) ) {
+		$comments_by_post_id[ $_comment->comment_post_ID ] = array();
+	}
+
+	$comments_by_post_id[ $_comment->comment_post_ID ][] = $_comment;
+
+	if ( ! isset( $last_comment_by_post_id[ $_comment->comment_post_ID ] ) ) {
+		$last_comment_by_post_id[ $_comment->comment_post_ID ] = $_comment->comment_date;
+	} elseif ( $last_comment_by_post_id[ $_comment->comment_post_ID ] < $_comment->comment_date ) {
+		$last_comment_by_post_id[ $_comment->comment_post_ID ] = $_comment->comment_date;
+	}
+}
+
+uasort(
+	$comments_by_post_id,
+	function( $a, $b ) use ( $last_comment_by_post_id ) {
+		return $last_comment_by_post_id[ $b->comment_post_ID ] <=> $last_comment_by_post_id[ $a->comment_post_ID ];
+	}
+);
+
+$args = array(
+	'style'            => 'ul',
+	'type'             => 'comment',
+	'callback'         => 'gth_discussion_callback',
+	'reverse_children' => false,
+);
+
+foreach ( $comments_by_post_id as $_post_id => $post_comments ) {
+	$original_id = Helper_Translation_Discussion::get_original_from_post_id( $_post_id );
+	if ( ! $original_id ) {
+		continue;
+	}
+	$original = GP::$original->get( $original_id );
+	$first_comment = reset( $post_comments );
+	?>
+	<h2><?php echo esc_html( $original->singular ); ?></h2>
+	<a href="<?php echo esc_attr( get_comment_link( $first_comment ) ); ?>">Go to Discussions page</a> | <a href="#after-post-<?php echo esc_attr( $_post_id ); ?>">Next ↓</a>
+	<div id="dashboard-comments-<?php echo esc_attr( $_post_id ); ?>" style="border-left: 2px solid #ccc; padding-left: 2em; scroll-behavior: smooth">
+
+	<?php
+
+	wp_list_comments( $args, $post_comments );
+	?>
+	</div>
+	<a name="after-post-<?php echo esc_attr( $_post_id ); ?>"></a>
+	<?php
+}
+
+?>
+
 <table id="translations" class="translations clear">
 	<thead>
 	<tr>
