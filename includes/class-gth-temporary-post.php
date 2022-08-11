30b73f852b948c17e56ab9450a3ac75e4c59184c
Alex Kirk <akirk@users.noreply.github.com>
2022-08-11 11:10:11 +0200

    Revert changes

--- includes/class-gth-temporary-post.php
+++ includes/class-gth-temporary-post.php
@@ -3,8 +3,6 @@
 class Gth_Temporary_Post {
 	public $ID;
 	public $comments_open = 'open';
-	public $comment_status = 'open';
-	public $filter = 'raw';
 	public function __construct( $post_id ) {
 		$this->ID = $post_id;
 	}
