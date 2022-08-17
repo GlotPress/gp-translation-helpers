2645a2c8cda005be89d09bb654df0dbe6121fd0f
Tosin Oguntuyi <tcubed03@gmail.com>
2022-08-17 22:40:03 +0100

    Show only first comment per original and the number of remaining comments

--- templates/discussions-dashboard.php
+++ templates/discussions-dashboard.php
@@ -47,25 +47,7 @@ $args = array(
 	'reverse_children' => false,
 );
 
-foreach ( $comments_by_post_id as $_post_id => $post_comments ) {
-	$original_id = Helper_Translation_Discussion::get_original_from_post_id( $_post_id );
-	if ( ! $original_id ) {
-		continue;
-	}
 
-	$original      = GP::$original->get( $original_id );
-	$first_comment = reset( $post_comments );
-	?>
-	<h2><?php echo esc_html( $original->singular ); ?></h2>
-	<a href="<?php echo esc_attr( get_comment_link( $first_comment ) ); ?>">Go to Discussions page</a> | <a href="#after-post-<?php echo esc_attr( $_post_id ); ?>">Next ↓</a>
-	<div id="dashboard-comments-<?php echo esc_attr( $_post_id ); ?>" style="border-left: 2px solid #ccc; padding-left: 2em; scroll-behavior: smooth">
-	<?php
-		wp_list_comments( $args, $post_comments );
-	?>
-	</div>
-	<a name="after-post-<?php echo esc_attr( $_post_id ); ?>"></a>
-	<?php
-}
 
 ?>
 
@@ -80,12 +62,18 @@ foreach ( $comments_by_post_id as $_post_id => $post_comments ) {
 	</thead>
 	<tbody>
 		<?php
-		foreach ( $comments as $discussion_comment ) {
-			$original_id = Helper_Translation_Discussion::get_original_from_post_id( $discussion_comment->comment_post_ID );
-			$original    = GP::$original->get( $original_id );
+		foreach ( $comments_by_post_id as $_post_id => $post_comments ) {
+			$original_id = Helper_Translation_Discussion::get_original_from_post_id( $_post_id );
+			if ( ! $original_id ) {
+				continue;
+			}
+
+			$original          = GP::$original->get( $original_id );
+			$first_comment     = reset( $post_comments );
+			$no_other_comments = count( $post_comments ) - 1;
 			echo '<tr>' .
 			 '<td>' . esc_html( $original->singular ) . '</td>' .
-			 '<td><a href="' . esc_url( get_comment_link( $discussion_comment ) ) . '">' . esc_html( $discussion_comment->comment_content ) . '</a></td>' .
+			 '<td><a href="' . esc_url( get_comment_link( $first_comment ) ) . '">' . esc_html( $first_comment->comment_content ) . ' + ' . $no_other_comments . ' other comments</a></td>' .
 			 '<td>' . esc_html( $discussion_comment->comment_author ) . '</td>' .
 			 '<td>' . esc_html( $discussion_comment->comment_date ) . '</td>' .
 			 '</tr>';
