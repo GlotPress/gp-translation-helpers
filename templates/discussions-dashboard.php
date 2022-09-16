745489cda630f53381c27b8f0a7e41118959d744
Alex Kirk <akirk@users.noreply.github.com>
2022-09-16 10:34:32 +0200

    Fix wrong variable name

--- templates/discussions-dashboard.php
+++ templates/discussions-dashboard.php
@@ -60,18 +60,12 @@ foreach ( $comments as $_comment ) {
 
 // If the referenced comment is not in the current batch of comments we need to re-add it.
 foreach ( $bulk_comments as $original_id => $_post_id ) {
-	if ( ! isset( $comments_by_post_id[ $_comment->comment_post_ID ] ) ) {
+	if ( ! isset( $comments_by_post_id[ $_post_id ] ) ) {
 		$linked_comment = $_comment->comment_content;
 		$parts          = wp_parse_url( $linked_comment );
 		$comment_id     = intval( str_replace( 'comment-', '', $parts['fragment'] ) );
 		if ( $comment_id ) {
-			$_comment = get_comment( $comment_id );
-			if ( $_comment ) {
-				$comments_by_post_id[ $_comment->comment_post_ID ][] = $_comment;
-				if ( ! isset( $latest_comment_date_by_post_id[ $_comment->comment_post_ID ] ) ) {
-					$latest_comment_date_by_post_id[ $_comment->comment_post_ID ] = $_comment->comment_date;
-				}
-			}
+			$comments_by_post_id[ $_post_id ][] = get_comment( $comment_id );
 		}
 	}
 }
