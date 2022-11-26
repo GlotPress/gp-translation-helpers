30fada267e37dd66b43c19460ef9f988b168b15f
Jesús Amieiro <1667814+amieiro@users.noreply.github.com>
2022-11-26 16:29:34 +0100

    Change the tab and the div when the user clicks in a tab

--- includes/class-gp-sidebar.php
+++ includes/class-gp-sidebar.php
@@ -14,15 +14,21 @@ class GP_Sidebar {
 	}
 
 	public static function add_tabs( $meta_sidebar, $defined_vars ) {
-		$tabs  = '<nav class="nav-sidebar">';
-		$tabs .= '<ul class="sidebar-tabs">';
-		$tabs .= '	<li class="current" data-tab="sidebar-tab-meta-' . $defined_vars['translation']->original_id . '">Meta</li>';
-		$tabs .= '	<li data-tab="sidebar-tab-discuss-' . $defined_vars['translation']->original_id . '">Discuss<span class="count">(5)</span></li>';
-		$tabs .= '	<li data-tab="sidebar-tab-history-' . $defined_vars['translation']->original_id . '">History<span class="count">(3)</span></li>';
-		$tabs .= '	<li data-tab="sidebar-tab-other-locales-' . $defined_vars['translation']->original_id . '">Other locales<span class="count">(12)</span></li>';
-		$tabs .= '</ul>';
+		$discussion_tab    = '<div class="meta discussion" id="sidebar-div-discussion-' . $defined_vars['translation']->original_id . '" style="display: none;">Discussion tab</div>';
+		$history_tab       = '<div class="meta history" id="sidebar-div-history-' . $defined_vars['translation']->original_id . '" style="display: none;">History tab</div>';
+		$other_locales_tab = '<div class="meta other-locales" id="sidebar-div-other-locales-' . $defined_vars['translation']->original_id . '" style="display: none;">Other locales tab</div>';
+		$tabs              = '<nav class="nav-sidebar">';
+		$tabs             .= '<ul class="sidebar-tabs">';
+		$tabs             .= '	<li class="current" data-tab="sidebar-tab-meta-' . $defined_vars['translation']->original_id . '">Meta</li>';
+		$tabs             .= '	<li data-tab="sidebar-tab-discussion-' . $defined_vars['translation']->original_id . '">Discuss<span class="count">(5)</span></li>';
+		$tabs             .= '	<li data-tab="sidebar-tab-history-' . $defined_vars['translation']->original_id . '">History<span class="count">(3)</span></li>';
+		$tabs             .= '	<li data-tab="sidebar-tab-other-locales-' . $defined_vars['translation']->original_id . '">Other locales<span class="count">(12)</span></li>';
+		$tabs             .= '</ul>';
 
 		$tabs .= $meta_sidebar;
+		$tabs .= $discussion_tab;
+		$tabs .= $history_tab;
+		$tabs .= $other_locales_tab;
 		$tabs .= '</nav>';
 
 		return $tabs;
--- js/editor.js
+++ js/editor.js
@@ -1,12 +1,40 @@
 /* global $gp */
+/* eslint camelcase: "off" */
 jQuery( function( $ ) {
 	$gp.editor.table.on( 'click', '.sidebar-tabs li', function() {
 		var tab = $( this );
 		var tabId = tab.attr( 'data-tab' );
+		var divId = tabId.replace( 'tab', 'div' );
+		var originalId = tabId.split( '-' ).pop();
+		change_visible_tab( tab );
+		change_visible_div( divId, originalId );
+	} );
+
+	/**
+	 * Hide all tabs and show one of them, the last clicked.
+	 *
+	 * @param {Object} tab The selected tab.
+	 */
+	function change_visible_tab( tab ) {
+		var tabId = tab.attr( 'data-tab' );
 		tab.siblings().removeClass( 'current' );
 		tab.parents( '.sidebar-tabs ' ).find( '.helper' ).removeClass( 'current' );
-
 		tab.addClass( 'current' );
+
 		$( '#' + tabId ).addClass( 'current' );
-	} );
+	}
+
+	/**
+	 * Hide all divs and show one of them, the last clicked.
+	 *
+	 * @param {string} tabId      The select tab id.
+	 * @param {number} originalId The id of the original string to translate.
+	 */
+	function change_visible_div( tabId, originalId ) {
+		$( '#sidebar-div-meta-' + originalId ).hide();
+		$( '#sidebar-div-discussion-' + originalId ).hide();
+		$( '#sidebar-div-history-' + originalId ).hide();
+		$( '#sidebar-div-other-locales-' + originalId ).hide();
+		$( '#' + tabId ).show();
+	}
 } );
