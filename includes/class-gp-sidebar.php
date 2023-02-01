77a5e454fb6026c65c43d80e3e32b1f5ddb5b8e3
Jesús Amieiro <1667814+amieiro@users.noreply.github.com>
2023-02-01 09:16:26 +0100

    Add the translation-row-editor-meta for the right sidebar

--- includes/class-gp-sidebar.php
+++ includes/class-gp-sidebar.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Routes: GP_Sidebar class
  *
@@ -10,37 +11,17 @@
 class GP_Sidebar {
 
 	public static function init() {
-		add_filter( 'gp_right_sidebar', array( static::class, 'add_tabs' ), 10, 2 );
+		add_filter(
+			'gp_tmpl_load_locations',
+			function ( $locations, $template, $args, $template_path ) {
+				if ( 'translation-row-editor-meta' === $template ) {
+					array_unshift( $locations, dirname( __FILE__, 2 ) . '/templates/gp-templates-overrides/' );
+				}
+
+				return $locations;
+			},
+			60,
+			4
+		);
 	}
-
-	/**
-	 * Adds a system with 4 tabs and divs to show the information in the right sidebar.
-	 *
-	 * @param string $meta_sidebar The HTML content for the meta sidebar.
-	 * @param array  $defined_vars The defined variables.
-	 *
-	 * @return string
-	 */
-	public static function add_tabs( $meta_sidebar, $defined_vars ) {
-		$id                = $defined_vars['translation']->row_id;
-		$discussion_tab    = '<div class="meta discussion" id="sidebar-div-discussion-' . $id . '"  data-row-id="' . $id . '" style="display: none;">Discussion tab</div>';
-		$history_tab       = '<div class="meta history" id="sidebar-div-history-' . $id . '"  data-row-id="' . $id . '" style="display: none;">History tab</div>';
-		$other_locales_tab = '<div class="meta other-locales" id="sidebar-div-other-locales-' . $id . '"  data-row-id="' . $id . '" style="display: none;">Other locales tab</div>';
-		$tabs              = '<nav class="nav-sidebar">';
-		$tabs             .= '<ul class="sidebar-tabs">';
-		$tabs             .= '	<li class="current tab-meta" data-tab="sidebar-tab-meta-' . $id . '" data-row-id="' . $id . '">Meta</li>';
-		$tabs             .= '	<li class="tab-discussion" data-tab="sidebar-tab-discussion-' . $id . '" data-row-id="' . $id . '">Discussion&nbsp;<span class="count">(0)</span></li>';
-		$tabs             .= '	<li class="tab-history" data-tab="sidebar-tab-history-' . $id . '" data-row-id="' . $id . '">History&nbsp;<span class="count">(0)</span></li>';
-		$tabs             .= '	<li class="tab-other-locales" data-tab="sidebar-tab-other-locales-' . $id . '" data-row-id="' . $id . '">Other&nbsp;locales&nbsp;<span class="count">(0)</span></li>';
-		$tabs             .= '</ul>';
-
-		$tabs .= $meta_sidebar;
-		$tabs .= $discussion_tab;
-		$tabs .= $history_tab;
-		$tabs .= $other_locales_tab;
-		$tabs .= '</nav>';
-
-		return $tabs;
-	}
-
 }
